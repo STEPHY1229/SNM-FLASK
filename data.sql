12c4f305f37b76d3e6b826aaf60176a61381d0d9
STEPHY1229 <stephyhallel2002@gmail.com>
2026-05-28 12:16:40 +0530

    data

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
--- Host: localhost    Database: psnm
+-- Host: localhost    Database: snm27db
 -- ------------------------------------------------------
 -- Server version	8.0.45
 
@@ -14,3 +14,100 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `filesdata`
+--
+
+DROP TABLE IF EXISTS `filesdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `filesdata` (
+  `filesid` int unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(20) DEFAULT NULL,
+  `filedata` longblob,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `userid` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`filesid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `filesdata_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `userdata` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filesdata`
+--
+
+LOCK TABLES `filesdata` WRITE;
+/*!40000 ALTER TABLE `filesdata` DISABLE KEYS */;
+INSERT INTO `filesdata` VALUES (1,'cmail.py',_binary 'mail_password=\'isjx rqrv tibl jamb\'\r\nimport smtplib \r\nfrom email.message import EmailMessage\r\ndef send_mail(to,subject,body):\r\n    server=smtplib.SMTP_SSL(\'smtp.gmail.com\',465)\r\n    server.login(\'stephyhallel2002@gmail.com\',mail_password)\r\n    msg=EmailMessage()\r\n    msg[\'FROM\']=\'stephyhallel2002@gmailcom\'\r\n    msg[\'TO\']=to\r\n    msg[\'SUBJECT\']=subject\r\n    msg.set_content(body)\r\n    server.send_message(msg)\r\n    print(\'MESSAGE SENT\')\r\n    server.close()','2026-05-23 12:18:14',1);
+/*!40000 ALTER TABLE `filesdata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notesdata`
+--
+
+DROP TABLE IF EXISTS `notesdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notesdata` (
+  `notesid` int unsigned NOT NULL AUTO_INCREMENT,
+  `notestitle` longtext,
+  `notes_description` longtext,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `userid` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`notesid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `notesdata_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `userdata` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notesdata`
+--
+
+LOCK TABLES `notesdata` WRITE;
+/*!40000 ALTER TABLE `notesdata` DISABLE KEYS */;
+INSERT INTO `notesdata` VALUES (1,'The Last Leaf','The Last Leaf is a short story about friendship.\r\n','2026-05-21 11:59:04',1),(3,'Python ','Easy to learn','2026-05-21 12:02:42',1),(4,'Flask ','Easy to design Framework.','2026-05-21 12:06:00',1),(5,'Electrical Machines','Learning the Mechanism of Electrical machines.','2026-05-22 12:39:52',1);
+/*!40000 ALTER TABLE `notesdata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userdata`
+--
+
+DROP TABLE IF EXISTS `userdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userdata` (
+  `userid` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `useremail` varchar(50) NOT NULL,
+  `userpassword` varchar(50) NOT NULL,
+  `userphone` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `useremail` (`useremail`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userdata`
+--
+
+LOCK TABLES `userdata` WRITE;
+/*!40000 ALTER TABLE `userdata` DISABLE KEYS */;
+INSERT INTO `userdata` VALUES (1,'Manju','stephyhallel2002@gmail.com','Manju@12345','9876543210'),(2,'Harshini','harshinisrinivasula2020@gmail.com','123','9638527410');
+/*!40000 ALTER TABLE `userdata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-28 12:04:18
